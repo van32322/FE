113d905d8e2946c6bfd555dc1ee3230680f7ec14
Tran Vi <vit76404@gmail.com>
2025-01-23 20:39:01 +0700

    header_almost_complete

--- src/pages/users/theme/header/index.js
+++ src/pages/users/theme/header/index.js
@@ -1,10 +1,49 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import "./style.scss"
 import { BsFacebook } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { BsMailbox } from "react-icons/bs";
 import { formatter } from "utils/formater";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { ROUTERS } from "utils/router";
 const Header = () => {
+    const [menus, setMenus] = useState([
+        {
+            name: "Trang chủ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: "Cửa hàng",
+            path: ROUTERS.USER.PRODUCTS,
+        },
+        {
+            name: "Sản phẩm",
+            path: "",
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: "Thịt",
+                    path: "",
+                },
+                {
+                    name: "Cá",
+                    path: "",
+                },
+                {
+                    name: "rau",
+                    path: "",
+                }
+            ]
+        },
+        {
+            name: "Bài viết",
+            path: "",
+        },
+        {
+            name: "Liên hệ",
+            path: "",
+        },
+    ])
     return (
         <>
             <div className="header_top">
@@ -48,9 +87,48 @@ const Header = () => {
 
             <div className="container">
                 <div className="row">
-                    <div className=" col-xl-3">LOGO</div>
-                    <div className=" col-xl-6">MENU</div>
-                    <div className=" col-xl-3">PHONE</div>
+                    <div className=" col-xl-3">
+                        <div className="header-logo">
+                            <h1>Team 5</h1>
+                        </div>
+                    </div>
+                    <nav className=" col-xl-6">
+                        <div className="header-menu">
+                            <ul>
+                                {
+                                    menus?.map((menu, menuKey) => (
+                                        <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                                            <Link to={menu?.path}>{menu?.name}</Link>
+                                        </li>
+
+                                    ))
+                                }
+
+                                {/* <li>
+
+                                    <ul>
+                                        <li>Thịt</li>
+                                        <li>Thịt</li>
+                                        <li>Thịt</li>
+                                    </ul>
+                                </li> */}
+                            </ul>
+                        </div>
+                    </nav>
+                    <div className=" col-xl-3">
+                        <div className="header-cart">
+                            <div className="header-cart-price">
+                                <span>{formatter(100000)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to={"#"}><AiOutlineShoppingCart />
+                                        <span>5</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
